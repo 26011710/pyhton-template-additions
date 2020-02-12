abfe9e7fd9820866f817bad5978317ba8ce60a6e
Charlotte <60974880+26011710@users.noreply.github.com>
2020-02-12 15:35:19 +0000

    Added hello world

---
+++ thegame.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 15:30:09 2020
+
+@author: hw011710
+"""
+
+def main():
+   # this is the main function
+   print("Hello World")
+   return 0
+
+if __name__ == "__main__":
+    main()
